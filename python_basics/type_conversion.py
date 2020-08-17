357e58a13e886103645f2dae04bad7dd88788bd4
akashrajput25 <55013687+akashrajput25@users.noreply.github.com>
2020-08-17 17:06:08 +0530

    type conversion in python

---
+++ python_basics/type_conversion.py
@@ -0,0 +1,10 @@
+'''
+birth_year = int(input('Birth Year: '))
+age = 2020 - birth_year
+print(type(age))
+print(age)
+'''
+
+#Task3
+weight_lbs = float(input('Enter your weight in Pounds'))
+print('Weight in KG is : ' + str(weight_lbs/2.20462))
