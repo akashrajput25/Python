d959687e861c8169fcff9bbd0754d6191b568f62
akashrajput25 <55013687+akashrajput25@users.noreply.github.com>
2020-08-17 17:04:49 +0530

    learnt basic of string

---
+++ python_basics/basic.py
@@ -0,0 +1,18 @@
+'''
+print('Akash Kumar Singh')
+print('o-----')
+print('  ||||')
+print('*' * 10)
+
+price = 10 
+price = 20
+rating =4.9
+name ='Akash'
+is_published = False
+print(price)
+'''
+#Task 1
+
+name = 'Jhon Smith'
+age = 20
+is_new = True
