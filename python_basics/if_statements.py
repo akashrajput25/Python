96b83c870af30cbdf17f607266c07ce1171db8e3
akashrajput25 <55013687+akashrajput25@users.noreply.github.com>
2020-08-17 22:22:34 +0530

    if statements

---
+++ python_basics/if_statements.py
@@ -0,0 +1,30 @@
+'''
+is_hot = False
+is_cold = False
+
+if is_hot : 
+    print('Have cold drink')
+
+elif is_cold:
+    print('Its a cold day, have tea')
+
+else:
+    print('Lovely day')
+
+print('enjoy your day')
+
+'''
+
+#Task6
+
+house_price = 1000
+
+good_credit = True
+
+if good_credit:
+    down_payment = 0.1 * house_price
+    print(f'Down payment of house = RS {down_payment}') 
+
+else:
+    down_payment = 0.2 * house_price
+    print(f'Down payment of house = RS {down_payment}') 
