683288f368f5ceacf2ef9d984bb5d9516bd4b53d
akashrajput25 <55013687+akashrajput25@users.noreply.github.com>
2020-08-17 22:22:07 +0530

    operator precendence

---
+++ python_basics/op_precedence.py
@@ -0,0 +1,12 @@
+x = (10 + 3) * 2 ** 2
+print(x)
+
+########### order of arithmetic operators ##############
+                # parenthesis
+                # exponential
+                # division
+                # multiplication
+                # addition
+                # subtraction
+
+
