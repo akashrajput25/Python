7ecdf8ef10900808714724929d557efb249c4d7c
akashrajput25 <55013687+akashrajput25@users.noreply.github.com>
2020-08-17 22:21:33 +0530

    completed string methods

---
+++ python_basics/Formatting_Strings.py
@@ -0,0 +1,6 @@
+first = 'Akash'
+last = 'Singh'
+message = first + ' ['+last+'] is a programmer'
+msg = f'{first} [{last}] is a programmer'
+print(message,'\n')
+print(msg)
---
+++ python_basics/string_methods.py
@@ -0,0 +1,8 @@
+course = 'Python is Cool'
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find('o'))
+print(course.replace('Cool','Awesome'))
+print('Python' in course)
+print(course.title())
