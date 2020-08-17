e2f1c89df699c135e47f9ddf58b90a0e9c7df07e
akashrajput25 <55013687+akashrajput25@users.noreply.github.com>
2020-08-17 22:20:54 +0530

    completed comparator operations

---
+++ python_basics/comparison_op.py
@@ -0,0 +1,21 @@
+'''
+temperature = 30
+
+if temperature > 30  or temperature == 30:
+    print('Hot day')
+else:
+    print('Not hot day')
+
+'''
+
+#Task7
+
+name = input('Enter your name')
+if len(name) <= 3:
+    print('Name must be more than 3 characters')
+
+elif len(name) >50:
+    print("name can't be more than 50 characters") 
+
+else:
+    print('Looks Good')
