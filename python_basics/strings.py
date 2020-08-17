746429f6032d1780ab2b66c1fe74282ed6547f5c
akashrajput25 <55013687+akashrajput25@users.noreply.github.com>
2020-08-17 17:05:48 +0530

    strings_basics in python

---
+++ python_basics/strings.py
@@ -0,0 +1,11 @@
+"""
+course = "Python's Course for Beginners"
+course = ''' Hello Akash,
+        How are you?'''
+print(course)
+print(course[0:4] + ' ' + course[-4:-1])
+"""
+
+#Task4
+name = 'Jennifer'
+print(name[1:-1])
