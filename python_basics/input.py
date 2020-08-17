b61ce8a9db778487f9dd262ebc68c953b0b74960
akashrajput25 <55013687+akashrajput25@users.noreply.github.com>
2020-08-17 17:05:15 +0530

    learnt to take input from user

---
+++ python_basics/input.py
@@ -0,0 +1,10 @@
+'''
+name = input('What is your Name ? ')
+print('Hi '+name)
+'''
+
+#Task 2
+name = input('What is your name ? ')
+color = input('What is your favourite color ? ')
+
+print(name + ' likes ' + color)
